4977cbeac4f3c0cfc90195e4d6aac9b7eea60cf1
Namiki Shimoo <naminori4mo@gmail.com>
2020-09-09 20:31:01 +0900

    fix wrong file

--- 0_libraries/8_SegTree.py
+++ 0_libraries/8_SegTree.py
@@ -1,56 +1,50 @@
-class UnionFind():
-    def __init__(self, n):
-        self.n = n
-        self.parents = [-1] * n
+# 0-indexed
+class SegTree:
+    def __init__(self, vals, seg_f, ide_ele):
+        n = len(vals)
+        self.seg_f = seg_f
+        self.ide_ele = ide_ele
+        self.leaf_n = 1 << (n-1).bit_length()
+        self.tree = [ide_ele]*(2*self.leaf_n-1)
+        for i in range(n):  # init leaf
+            self.tree[i+self.leaf_n-1] = vals[i]
+        for i in range(self.leaf_n-2, -1, -1):  # bottom-up
+            self.tree[i] = self.seg_f(self.tree[2*i+1], self.tree[2*i+2])
 
-    def find(self, x):
-        if self.parents[x] < 0:
-            return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
+    def update(self, i, x):
+        i += (self.leaf_n-1)
+        self.tree[i] = x
+        while i > 1:
+            i = (i-1)//2
+            self.tree[i] = self.seg_f(self.tree[2*i+1], self.tree[2*i+2])
 
-    def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
+    # [l,r)
+    def query(self, l, r):
+        v_l = self.ide_ele
+        v_r = self.ide_ele
+        l += (self.leaf_n-1)
+        r += (self.leaf_n-1)
+        while l < r:
+            if l&1 == 0:
+                v_l = self.seg_f(v_l, self.tree[l])
+            if r&1 == 0:
+                v_r = self.seg_f(v_r, self.tree[r-1])
+                r -= 1
+            l >>= 1
+            r >>= 1
+        return self.seg_f(v_l ,v_r)
 
-        if x == y:
-            return
 
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
+## 
+vl = [1,4,6,8,9]
 
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
+def add(a,b): return a+b
+st = SegTree(vl,add,0)
+st = SegTree(vl,(lambda a,b: a+b),0)
+st = SegTree(vl,min,10**10)
 
-    def size(self, x):
-        return -self.parents[self.find(x)]
-
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
-
-    def members(self, x):
-        root = self.find(x)
-        return [i for i in range(self.n) if self.find(i) == root]
-
-    def roots(self):
-        return [i for i, x in enumerate(self.parents) if x < 0]
-
-    def group_count(self):
-        return len(self.roots())
-
-    # too slow
-    def all_group_members(self):
-        return {r: self.members(r) for r in self.roots()}
-
-    def get_all_members(self):
-        pare_d = {}
-        for i in range(self.n):
-            parent = self.find(i)
-            pare_d.setdefault(parent,[])
-            pare_d[parent].append(i)
-        return list(pare_d.values())
-
-    def __str__(self):
-        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
-
+print(st.query(2,4))
+st.update(4,-4)
+print(st.query(2,5))
+st.update(0,-10)
+print(st.query(0,5))
