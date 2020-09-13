27913261178e2370c2347ce7acd3e47a88c75efa
Namiki Shimoo <naminori4mo@gmail.com>
2020-09-13 16:05:39 +0900

    solve problems

--- 2_kakomon/abc042-125/abc124_c.cpp
+++ 2_kakomon/abc042-125/abc124_c.cpp
@@ -8,7 +8,7 @@ typedef pair<ll,ll> P;
 #define REP(i,n) for(ll i = 0 ; i < n ; i++) // for i in range(b)
 #define FORD(i,a,b) for(ll i = a ; i > b ; i--) // for i in range(a,b,-1)
 #define ALL(x) x.begin(),x.end()
-#define debug(x) cerr << #x << ": " << x << '\n'
+#define debug(x) cerr << #x << ": " << x << '\n' // Too slow. Please delete it when submtting the code.
 
 const int IINF = 1'001'001'001;
 const ll INF = 1'001'001'001'001'001'001ll;
@@ -16,7 +16,22 @@ const int MOD = 1'000'000'007;
 
 
 void solve(){
-
+    string s; cin >> s;
+    int ans = 0;
+    int cnt = 0;
+    REP(i,s.size()){
+        // debug(i);
+        if(i%2==0 && s[i]=='0') cnt+=1;
+        else if(i%2==1 && s[i]=='1') cnt+=1;
+    }
+    ans = cnt;
+    cnt = 0;
+    REP(i,s.size()){
+        if(i%2==0 && s[i]=='1') cnt+=1;
+        else if(i%2==1 && s[i]=='0') cnt+=1;
+    }
+    ans = min(ans,cnt);
+    cout << ans << endl;
 }
 
 int main(){
