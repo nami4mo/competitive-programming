12905b90066b36fed8e49764d309cd50de0eb565
Namiki Shimoo <naminori4mo@gmail.com>
2020-12-05 20:37:30 +0900

    splve problems

--- 0_libraries/cpp_libs/3_combination.cpp
+++ 0_libraries/cpp_libs/3_combination.cpp
@@ -1,40 +1,4 @@
-#include <bits/stdc++.h>
-#if __has_include(<atcoder/all>)
-    #include <atcoder/all>
-    using namespace atcoder;
-#endif
-using namespace std;
-namespace defines{
-    typedef long long ll;
-    typedef pair<ll,ll> P;
-    #define FOR(i,a,b) for(ll i = a ; i < b ; i++) // for i in range(a,b)
-    #define REP(i,n) for(ll i = 0 ; i < n ; i++) // for i in range(b)
-    #define FORD(i,a,b) for(ll i = a ; i > b ; i--) // for i in range(a,b,-1)
-    #define ALL(x) x.begin(),x.end()
-    template<class T> vector<T> make_vec(size_t a){return vector<T>(a);}
-    template<class T, class... Ts> auto make_vec(size_t a, Ts... ts){ return vector<decltype(make_vec<T>(ts...))>(a, make_vec<T>(ts...));}
-
-    /* for debug print */
-    #define DEBUG(x) cerr << #x << ": " << x << '\n'
-    #define DEBUGP(x) cerr << #x << ": (" << x.first << ", " << x.second << ")" << '\n' 
-    #define DEBUGL(xl) dbgl_f(#xl,xl)
-    #define DEBUGLP(xl) dbglp_f(#xl,xl)
-    #define DEBUGLL(xll) dbgll_f(#xll,xll)
-    #define DEBUGM(xl) dbgm_f(#xl,xl)
-    template<class T> void dbgl_f(string name, vector<T> xl){cerr<<name<<": "; for(T x: xl) cerr<<x<<" "; cerr << '\n';}
-    template<class T, class U> void dbgm_f(string name, map<T,U> xl){cerr<<name<<": ";for(auto x: xl)cerr<<"("<<x.first<<": "<<x.second<<") ";cerr<<'\n';}
-    template<class T> void dbglp_f(string name, vector<T> xl){cerr<<name<<": ";for(T x: xl)cerr<<"("<<x.first<<", "<< x.second<<") ";cerr<<'\n';}
-    template<class T> void dbgll_f(string name, vector<vector<T>> xll){
-        cerr<< name << ": " << '\n'; 
-        for(vector<T> xl: xll){
-            for(T x : xl) cerr << x << " ";
-            cerr << '\n';
-        }
-    }
-}
-using namespace defines;
-
-
+#include "xx_def.hpp"
 
 const int MOD = 1000000007;
 const int MAX = 510000;
@@ -55,23 +19,3 @@ long long com(int n, int k){
     if (n < 0 || k < 0) return 0;
     return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
-
-void solve(){
-    ll n,k; cin >> n >> k;
-    com_init();
-    ll rem = k%n;
-    if(n>k){
-        cout << com(n+k-1,k) << endl;
-    }
-    else{
-        cout << com(n,rem) << endl;
-    }
-}
-
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    solve();
-    return 0;
-}
-
