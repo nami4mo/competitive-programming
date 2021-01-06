0ed749d072a9efcff3b961a8d359bdc22fc59ced
Namiki Shimoo <naminori4mo@gmail.com>
2021-01-06 21:46:58 +0900

    update

--- 0_libraries/cpp_libs/xx_def.hpp
+++ 0_libraries/cpp_libs/xx_def.hpp
@@ -9,22 +9,20 @@ typedef pair<ll,ll> P;
 #define FORD(i,a,b) for(ll i = a ; i > b ; i--) // for i in range(a,b,-1)
 #define ALL(x) x.begin(),x.end()
 
-// #define DEBUG(x) cerr << #x << ": " << x << '\n'
-#define DEBUGP(x) cerr << #x << ": (" << x.first << ", " << x.second << ")" << '\n' 
-#define DEBUGL(xl) dbgl_f(#xl,xl)
-#define DEBUGLP(xl) dbglp_f(#xl,xl)
-#define DEBUGLL(xll) dbgll_f(#xll,xll)
-#define DEBUGM(xl) dbgm_f(#xl,xl)
-template<class T> void dbgl_f(string name, vector<T> xl){cerr<<name<<": "; for(T x: xl) cerr<<x<<" "; cerr << '\n';}
-template<class T, class U> void dbgm_f(string name, map<T,U> xl){cerr<<name<<": ";for(auto x: xl)cerr<<"("<<x.first<<": "<<x.second<<") ";cerr<<'\n';}
-template<class T> void dbglp_f(string name, vector<T> xl){cerr<<name<<": ";for(T x: xl)cerr<<"("<<x.first<<", "<< x.second<<") ";cerr<<'\n';}
-template<class T> void dbgll_f(string name, vector<vector<T>> xll){
-    cerr<< name << ": " << '\n'; 
-    for(vector<T> xl: xll){
-        for(T x : xl) cerr << x << " ";
-        cerr << '\n';
-    }
-}
+/* for debug */
+#define DEBUG(x) dbg(#x,x)
+template<class T> void dbg(string name, T x){cerr<<name<<": "<<x<<"\n";}
+template<> void dbg<P>(string name, P x){cerr<<name<<": ("<<x.first<<","<<x.second<<")\n";}
+template<class T> void dbg(string name, vector<T> xl){cerr<<name<<": "; for(T x: xl) cerr<<x<<" "; cerr<<'\n';}
+template<> void dbg<P>(string name, vector<P> xl){cerr<<name<<": "; for(P x:xl){cerr<<"("<<x.first<<","<<x.second<<"), ";}cerr<<"\n";}
+template<class T> void dbg(string name, vector<vector<T>> xl){ cerr<<name<<": \n"; int ml=1;for(vector<T> row: xl){for(T x:row){stringstream sstm; sstm<<x; ml=max(ml,(int)sstm.str().size());}}; for(vector<T> row: xl){{for(T x:row) cerr<<std::right<<std::setw(ml+1)<<x;} cerr << '\n';}}
+template<class T> void dbg(string name, set<T> xl){cerr<<name<<": "; for(T x:xl)cerr<<x<<" ";cerr<<'\n';}
+template<class T> void dbg(string name, multiset<T> xl){cerr<<name<<": "; for(T x:xl)cerr<<x;cerr<<'\n';}
+template<class T> void dbg(string name, unordered_set<T> xl){cerr<<name<<": "; for(T x:xl)cerr<<x<<" ";cerr<<'\n';}
+template<class T> void dbg(string name, unordered_multiset<T> xl){cerr<<name<<": "; for(T x:xl)cerr<<x;cerr<<'\n';}
+template<class T, class U> void dbg(string name, map<T,U> xl){cerr<<name<<": \n"; for(auto &[k,v]:xl)cerr<<"  "<<k<<": "<<v<<'\n';}
+template<class T, class U> void dbg(string name, unordered_map<T,U> xl){cerr<<name<<": \n"; for(auto &[k,v]:xl)cerr<<"  "<<k<<": "<<v<<'\n';}
+
 const int IINF = 1'001'001'001;
 const ll INF = 1'001'001'001'001'001'001ll;
 const int MOD = 1'000'000'007;
